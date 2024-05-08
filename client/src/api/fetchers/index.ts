9658dbc5eb0e53e2d672c80646c6d6ab3ab05921
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-05-07 22:40:10 -0400

    👔 Export fetchers

---
+++ client/src/api/fetchers/index.ts
@@ -0,0 +1,3 @@
+export { default as createMessage } from "./createMessage";
+export { default as deleteMessageById } from "./deleteMessageById";
+export { default as getAllMessages } from "./getAllMessages";

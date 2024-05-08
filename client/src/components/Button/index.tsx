44c0aa68125c8b24b5da03b53fe4f42e6af67774
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-05-07 22:40:37 -0400

    ✨ Add `Button` component

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./index.module.scss";
+
+type Props = {
+	variant?: "primary";
+	addedClassName?: string;
+} & React.ComponentProps<"button">;
+
+export default function Button({ variant = "primary", addedClassName, children, ...props }: Props) {
+	return (
+		<button
+			className={[styles["button"], addedClassName ? addedClassName : ""].join(" ")}
+			data-variant={variant}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
